00a79c388322021b9e98515c5e7131d9d608c55c
jusufjulma <jusaleander@hotmail.com>
2019-10-26 18:03:26 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Home Network System
-
-Collection of more or less handy tools to have available on home network. Starting with fairly simple converting tool.
-Made with React.
-
---
+++ precache-manifest.1a3d633237b83c1ad0c3e1d419cd6794.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c1117dcff46f65c3b5b",
+    "url": "/hometools/static/js/runtime~main.8c1117dc.js"
+  },
+  {
+    "revision": "6792f398b369b51b9543",
+    "url": "/hometools/static/js/main.6792f398.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/hometools/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "6792f398b369b51b9543",
+    "url": "/hometools/static/css/main.9aa135d3.chunk.css"
+  },
+  {
+    "revision": "22a8c7306afd6fd9f632820f9175e96e",
+    "url": "/hometools/index.html"
+  }
+];
